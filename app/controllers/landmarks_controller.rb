b9be2259235ab7ea0ed661c8a340052ab63847d9
Barbara Washington <barbara.m.washington@gmail.com>
2018-07-26 18:45:17 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,9 +1,9 @@
 class LandmarksController < ApplicationController
 
-  # get '/landmarks' do
-  #     @landmarks = Landmark.all
-  #     erb :'/landmarks/index'
-  #   end
+  get '/landmarks' do
+      @landmarks = Landmark.all
+      erb :'/landmarks/index'
+    end
 
     get '/landmarks/new' do
     erb :'/landmarks/new'
