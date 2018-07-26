87c827b441c5b3604885fab3294e18c7ccaee1ee
Barbara Washington <barbara.m.washington@gmail.com>
2018-07-26 18:47:58 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -33,14 +33,14 @@ class FiguresController < ApplicationController
     @landmarks = @figure.landmarks
     erb :'figures/show'
   end
-  #
-  # get '/figures/:id/edit' do
-  #   @figure = Figure.find(params[:id])
-  #   @titles = Title.all
-  #   @landmarks = Landmark.all
-  #
-  #   erb :'figures/edit'
-  # end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    @titles = Title.all
+    @landmarks = Landmark.all
+
+    erb :'figures/edit'
+  end
   #
   # patch '/figures/:id' do
   #   @figure = Figure.find(params[:id])
