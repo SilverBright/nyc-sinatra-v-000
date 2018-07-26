31cd46b2a2ab3825e66b79c6c23d6c5bc68768d5
Barbara Washington <barbara.m.washington@gmail.com>
2018-07-26 18:46:44 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,10 +1,5 @@
 class FiguresController < ApplicationController
 
-  get '/figures' do
-    @figures = Figure.all
-    erb :'/figures/index'
-  end
-
   get '/figures/new' do
     @titles = Title.all
     @landmarks = Landmark.all
