e1c2a25ca8614535246f57f8ecd0d0bb60ad677b
Barbara Washington <barbara.m.washington@gmail.com>
2018-07-26 18:27:33 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -11,37 +11,39 @@ class FiguresController < ApplicationController
     erb :'/figures/new'
   end
 
+  post '/figures' do
+     @figure = Figure.create(name: params[:figure][:name])
+
+       if !params[:title][:name].empty?
+         @figure.titles << Title.create(name: params[:title][:name])
+       end
+
+       if !params[:figure][:title_ids].nil? && !params[:figure][:title_ids].empty?
+         params[:figure][:title_ids].each do |title_id|
+           @figure.titles << Title.find(title_id)
+         end
+       end
+
+       if !params[:landmark][:name].empty?
+         @figure.landmarks << Landmark.create(params[:landmark])
+       end
+
+
+       if !params[:figure][:landmark_ids].nil? && !params[:figure][:landmark_ids].empty?
+         #binding.pry
+         params[:figure][:landmark_ids].each do |landmark_id|
+           @figure.landmarks << Landmark.find(landmark_id)
+         end
+       end
+       @figure.save
+       redirect :"/figures/#{@figure.id}"
+   end
+
   get '/figures/:id' do
    @figure =  Figure.find(params[:id])
    erb :'figures/show'
  end
 
- post '/figures' do
-    @figure = Figure.create(name: params[:figure][:name])
 
-      if !params[:title][:name].empty?
-        @figure.titles << Title.create(name: params[:title][:name])
-      end
-
-      if !params[:figure][:title_ids].nil? && !params[:figure][:title_ids].empty?
-        params[:figure][:title_ids].each do |title_id|
-          @figure.titles << Title.find(title_id)
-        end
-      end
-
-      if !params[:landmark][:name].empty?
-        @figure.landmarks << Landmark.create(params[:landmark])
-      end
-
-
-      if !params[:figure][:landmark_ids].nil? && !params[:figure][:landmark_ids].empty?
-        #binding.pry
-        params[:figure][:landmark_ids].each do |landmark_id|
-          @figure.landmarks << Landmark.find(landmark_id)
-        end
-      end
-      @figure.save
-      redirect :"/figures/#{@figure.id}"
-  end
 
 end
